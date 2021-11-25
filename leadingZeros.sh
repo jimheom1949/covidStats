128b240b235adf90926347df7914930e59ebc80a
jimheom1949 <jim.heom@gmail.com>
2021-11-25 00:45:18 -0800

    fix deleted file message

--- leadingZeros.sh
+++
@@ -1,6 +0,0 @@
-#!bash
-
-awk 'match($0, /[0-9]+/){ x = substr( $0, RSTART, RLENGTH )
-printf("%s %03d\n", $1, x)
-}'
-
--- loadStockData.sh
+++
@@ -1,5 +0,0 @@
-echo ' 
-Cases and COVID-19 deaths
-
-The new confirmed and presumptive COVID-19 cases reported today are in the following counties: Baker (7), Benton (16), Clackamas (91), Clatsop (7), Columbia (15), Coos (23), Crook (2), Curry (8), Deschutes (111), Douglas (49), Gilliam (1), Grant (1), Harney (5), Hood River (3), Jackson (54), Jefferson (12), Josephine (28), Klamath (16), Lake (13), Lane (62), Lincoln (17), Linn (45), Malheur (12), Marion (79), Morrow (3), Multnomah (136), Polk (27), Sherman (1), Tillamook (8), Umatilla (27), Union (1), Wallowa (2), Wasco (7), Washington (100) and Yamhill (44).
-' |clip
